e92b976c45346233ba4f2a1f2908b5e47c39a362
Shubhamsavani <20it126@charusat.edu.in>
2022-09-14 12:35:52 +0530

    Adding Navbar, Footer, StudentList- with edit/delete buttons

--- studentmanagementsystemfrontend/src/components/StudentList.js
+++ studentmanagementsystemfrontend/src/components/StudentList.js
@@ -1,7 +1,23 @@
-import React from 'react'
-import { Container, Table } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import { Card, Container, Table, ButtonGroup, Button} from 'react-bootstrap'
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  let getStudents = () => {
+    axios
+      .get("http://localhost:8080/listStudents")
+      .then((response) => setStudents(response.data))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="my-3">
         <Container>
@@ -13,23 +29,29 @@ export default function StudentList() {
           <th>Student Id</th>
           <th>Student Name</th>
           <th>Student Address</th>
+          <th>Edit/Delete</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-        </tr>
+      {students.length === 0 ? (
+                <tr>
+                  <td colSpan={3}>{students.length} Studnets Available!!!</td>
+                </tr>
+              ) : (
+                students.map((student)=>
+                <tr>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
+                )
+              )}
       </tbody>
     </Table>
     </Card.Body>
