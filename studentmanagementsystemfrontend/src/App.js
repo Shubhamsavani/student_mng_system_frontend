a489975219ef734594c993a4666b11f15f0a1657
Shubhamsavani <20it126@charusat.edu.in>
2022-09-14 12:04:56 +0530

    Initial UI

--- studentmanagementsystemfrontend/src/App.js
+++ studentmanagementsystemfrontend/src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Student from './components/Student';
+import StudentList from './components/StudentList';
+
 
 function App() {
   return (
